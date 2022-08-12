b395206da437d185a8bf1dc145ea7fbdbea6d12f
Luis David Lopera <luis.lopera28@gmail.com>
2022-08-12 10:16:22 -0500

    Conection of db and creation of CRUD

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")] // esto es otro DataAnnotations que sirve apar definar la cantidad maxima de caracteres 
+        [Required(ErrorMessage ="el campo {0} es obligatorio.")]// un DataAnnotations se hace para validar es decir no me deja crear un pais sin un nombre 
+        // si el required lo dejamos asi [Required] el usuario le va aparecer un mensaje que no va a entender
+        // es mejor ponerlo con parentesis
+        [Display(Name = "País")]
+        public string Name { get; set; }
+
+        
+
+
+    }
+}
